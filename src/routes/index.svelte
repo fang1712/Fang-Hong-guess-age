d4cbb362c4d88d47cbf9949910d2659114f7347f
fang1712 <fanghong1712@gmail.com>
2021-12-19 04:07:40 +0000

    Completed

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    let name = "";
+    let age = "";
+   
+    async function guessage() {
+  fetch(' https://api.agify.io/?name=' + name)
+	.then((response) => response.json())
+	.then((data) => {
+  	age=data.age;
+	});
+}
+</script>
+
+<h1>Guess Age</h1>
+
+<form on:submit|preventDefault={guessage}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+<p>Age: {age}</p>
